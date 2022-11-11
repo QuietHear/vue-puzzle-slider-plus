05961c286d03cf393e67dd200238222a94520820
yps <1106728884@qq.com>
2022-11-11 18:22:30 +0800

    移除调试代码

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,16 @@
 */
 /*
  * @LastEditors: aFei
- * @LastEditTime: 2022-11-11 17:23:36
+ * @LastEditTime: 2022-11-11 18:10:21
 */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
+  esbuild: {
+    drop: ['console', 'debugger']
+  },
   build: {
     // 打包输出的目录路径
     outDir: 'dist',
@@ -18,13 +21,6 @@ export default defineConfig({
       entry: path.resolve(__dirname, "./lib/index.js"), //指定组件编译入口文件
       name: "vuePuzzleSliderPlus",
       fileName: "vue-puzzle-slider-plus",
-    },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      }
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
@@ -45,4 +41,4 @@ export default defineConfig({
       }
     }
   }
-});
+}));
